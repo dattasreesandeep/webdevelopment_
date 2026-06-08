0f40b1538ea60a22d9794478311cc9b4980b224a
Dattasree Sandeep <dattasreesandeep7@gmail.com>
2026-06-08 10:33:46 +0530

    Updated Dice program

--- Dice-Challenge/index.js
+++ Dice-Challenge/index.js
@@ -1,11 +1,8 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomNumber1 = Math.floor(Math.round()*6)+1;
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
 
-var randomImageSource1= 'image/dice' + randomNumber1 + '.png';
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource1);
-
-
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
@@ -13,13 +10,12 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Play 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
 else {
-  document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").innerHTML = "Draw!";
 }
